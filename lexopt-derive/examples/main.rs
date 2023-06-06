5fb7cb508d8bd4f637c8efe2a44bedd70f1ab8bb
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-06-06 22:21:08 +0200

    feat: add first PoC of the library
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- lexopt-derive/examples/main.rs
+++ lexopt-derive/examples/main.rs
@@ -1,6 +1,13 @@
-use lexopt_derive::{Parser, SubCommand};
+use lexopt_derive::{cli, help, Parser, SubCommand};
+use lexopt_helper::prelude::{CLIFlag, CLiCommand};
 
 #[derive(Parser)]
+#[cli(
+    name = "es",
+    about = "Just another command to manage the command line arguments",
+    version = "0.0.1",
+    author = "Vincenzo Palazzo <vincenzopalazzo@member.fsf.org>"
+)]
 pub struct CliArgs {
     #[subcommand]
     pub install: Install,
@@ -9,8 +16,29 @@ pub struct CliArgs {
 }
 
 #[derive(Debug, SubCommand)]
+#[cli(about = "Install somethings, but we do not know what")]
 pub struct Install {
     pub name: String,
+}
+
+#[help(CliArgs)]
+fn help<C: CLiCommand, F: CLIFlag>(top_level: Option<C>, sucommands: Vec<C>, flags: Vec<F>) {
+    if let Some(command) = top_level {
+        println!("{}   {} ", command.name(), command.description());
+        println!();
+        println!("TODO add usage example");
+    }
+    for command in sucommands {
+        println!();
+        println!("  {}        {}", command.name(), command.description());
+        help(None, command.subcommands::<C>(), command.flags::<F>());
+    }
+
+    for flag in flags {
+        println!();
+        println!();
+        println!("-{} | --{}", flag.short(), flag.long());
+    }
 }
 
 fn main() {
--- lexopt-derive/src/parser.rs
+++ lexopt-derive/src/parser.rs
@@ -1,14 +1,16 @@
 use kproc_parser::kparser::{KParserError, KParserTracer};
 use kproc_parser::proc_macro::TokenStream;
-use kproc_parser::rust::ast_nodes::StructToken;
+use kproc_parser::rust::ast_nodes::{FieldToken, StructToken};
 use kproc_parser::rust::kparser::RustParser;
 use kproc_parser::trace;
 
 use crate::Tracer;
 
 pub struct ParserInfo {
-    pub subcommands: Vec<String>,
-    pub flags: Vec<String>,
+    pub subcommands: Vec<FieldToken>,
+    pub flags: Vec<FieldToken>,
+    pub custom_help: bool,
+    pub custom_parse: bool,
 }
 
 impl ParserInfo {
@@ -28,10 +30,14 @@ pub fn parse(stream: TokenStream) -> TokenStream {
     let tracer = Tracer {};
     let parser = RustParser::with_tracer(&tracer);
     let ast = parser.parse_struct(&stream);
-    let Ok(parser_impl) = generate_parser(ast, &tracer) else {
-       todo!()
-    };
-    parser_impl.to_tokens_stream()
+    let parser_impl = generate_parser(ast, &tracer);
+    if let Err(err) = parser_impl {
+        err.emit();
+        panic!();
+    }
+    // SAFETY: it is safe to unwrap because
+    // we check and mange the error before.
+    parser_impl.unwrap().to_tokens_stream()
 }
 
 pub fn generate_parser<T: KParserTracer>(
@@ -41,13 +47,15 @@ pub fn generate_parser<T: KParserTracer>(
     let mut info = ParserInfo {
         subcommands: vec![],
         flags: vec![],
+        custom_help: false,
+        custom_parse: false,
     };
     for field in ast.fields {
         trace!(tracer, "{field}");
         if field.attrs.contains_key("subcommand") {
-            info.subcommands.push(field.to_string());
+            info.subcommands.push(field);
         } else {
-            info.subcommands.push(field.to_string());
+            info.subcommands.push(field);
         }
     }
     Ok(info)
